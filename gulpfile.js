6b3d8c0608cd92d2857d157ea5e6e1d22248f78f
Hyonkyong Cho <hyonkyong.cho@44383-501.auckland.natcoll.ac.nz>
2015-10-29 12:07:15 +1300

    speldnz 1st day on class

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var sourcemaps = require('gulp-sourcemaps');
  
  
 gulp.task('sass', function () {
-  gulp.src('.sass/**/*.scss')
+  gulp.src('sass/**/*.scss')
   	.pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
@@ -17,12 +17,12 @@ gulp.task('sass', function () {
             cascade: false
         }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('.css'));
+    .pipe(gulp.dest('css'));
 
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('.sass/**/*.scss', ['sass']);
+  gulp.watch('sass/**/*.scss', ['sass']);
 });
 
 
