0d77dc72beeb10bb9bb4bd8478cef347739801d9
Martin Cho <chadcho82@hotmail.com>
2015-10-30 01:42:53 +1300

    section2

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,35 @@
 $(function(){
 	// console.log('hi');
+	$('.menu').on("click", function() {
+
+		$('#mobile-nav').removeClass("hidden");
+		$('.menu').addClass('hidden');
+		$('.close').removeClass('hidden');
+
+	});
+
+	$('.close').on("click", function() {
+
+		$('#mobile-nav').addClass("hidden");
+		$('.close').addClass('hidden');
+		$('.menu').removeClass('hidden');
+
+	});
+
+	$('.learning').on("click",function(e) {
+
+		if($(this).on("click")){
+			$('.learning').css('flex','0 0 15em')
+		}
+			e.preventDefault();
+
+			$('#learning-sub-menu').removeClass("hidden");
+		if($('#learning-sub-menu').on("click")) {
+			$(this).unbind();
+		} 
+		
+	});
+
+
+	
 });
