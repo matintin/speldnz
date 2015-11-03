1709a27bd296ad0c8d634f5f2e3f6aa3435ed2b6
Martin Cho <chadcho82@hotmail.com>
2015-11-04 02:07:52 +1300

    mock up

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,7 @@ $(function(){
 	// console.log('hi');
 	var isOpen = false;
 
+//-------------------------menu----------------------------------
 	$('.menu').on("click", function() {
 
 		if(isOpen == false) {
@@ -25,6 +26,9 @@ $(function(){
 
 	});
 
+//-------------------------menu----------------------------------
+
+//---------------------learning-submenu--------------------------
 	var isLearningOpen = false;
 
 
@@ -48,6 +52,10 @@ $(function(){
 		}	
 		
 	});
+//---------------------learning-submenu--------------------------
+
+
+//---------------------search------------------------------------	
 	var isSearchOpen = false;
 	$('.search').on("click",function() {
 		if(isSearchOpen == false) {
@@ -62,12 +70,42 @@ $(function(){
 			isSearchOpen = false;
 		}
 	});
+//---------------------search------------------------------------
+
+//---------------------reading-assist----------------------------
+
+	$('.off-box').on("click",function() {
+		$('.circle img').attr("src","asset/icons/circle-on.png");
+		$('.on-box').css("border","none").css("background-color","rgba(0,0,0,0)").css("color","#fff").css("font-weight","100");
+		$('.off-box').css("background-color","#fff").css("border","1px #fff solid").css("border-radius","3px").css("color","tomato");
+		$('.on-off-wrap').css("background-color","#CDDC39").css("border","2px #CDDC39 solid");
+	});
+
+	$('.on-box').on("click",function() {
+		$('.circle img').attr("src","asset/icons/circle-off.png");
+		$('.off-box').css("border","none").css("background-color","rgba(0,0,0,0)").css("color","#fff").css("font-weight","100");
+		$('.on-box').css("background-color","#fff").css("border","1px #fff solid").css("border-radius","3px").css("color","tomato");
+		$('.on-off-wrap').css("background-color","#888").css("border","2px #888 solid");
+	});
+var isBottomOpen = false;
+
+	$('.reading-assist-top').on("click",function() {
+		if(isBottomOpen == false) {
+
+			$('.reading-assist-bottom').css("display","flex");
+			isBottomOpen = true;
+
+		}else {
+
+			$('.reading-assist-bottom').css("display","none");
+			isBottomOpen =false;
+		}
+	});
+//---------------------reading-assist----------------------------
+
+});//jquery
 
 //------------------image-slider----------------------------------
-
-//------------------image-slider----------------------------------
-	
-});
 
 $(document).ready(function(){
   $('.image-slider').slick({
@@ -76,5 +114,6 @@ $(document).ready(function(){
 	  autoplay: true,
 	  autoplaySpeed: 2000,
   });
-  console.log("hi");
 });
+//------------------image-slider----------------------------------
+	
